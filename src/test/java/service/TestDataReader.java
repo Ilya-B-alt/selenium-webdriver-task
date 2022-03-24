052d6e242301fa7a7e9f45ef70a33b929d7b27ed
Ilya Babaryka <boboriko2010@mail.ru>
2022-03-24 19:25:49 +0300

    replaced ResourceBundle with PropertyResourceBundle

--- src/test/java/service/TestDataReader.java
+++ src/test/java/service/TestDataReader.java
@@ -1,10 +1,10 @@
 package service;
 
-import java.util.Locale;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 public class TestDataReader {
-    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"), Locale.US);
+    private static PropertyResourceBundle resourceBundle = (PropertyResourceBundle) PropertyResourceBundle.getBundle(System.getProperty("environment"));
 
     public static String getTestData (String key) {
         return resourceBundle.getString(key);
